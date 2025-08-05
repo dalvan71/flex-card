9126f389b113ebf87d506588f4567c64921c8d78
dalvan71 <dalva.gehm@escola.pr.gov.br>
2025-08-05 16:33:11 -0300

    05/08

---
+++ assets/perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'jogos',
+    'melhor jogo',
+    'good offor'
+)
+
+
+criaCartao(
+    'esportes',
+    'melhor lutador',
+    'anderson silva'
+)
+
+criaCartao(
+    'carros',
+    'carros mais rapidos',
+    'relampago markinhos'
+)
